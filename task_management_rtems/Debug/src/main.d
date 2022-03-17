1ce20bb724e83b061de4597c023e4c824df859ee
Christian Mariscal <chr.mariscal10@gmail.com>
2022-03-17 23:32:40 +0100

    Inicializacion del repositorio

---
+++ task_management_rtems/Debug/src/main.d
@@ -0,0 +1,7 @@
+src/main.d src/main.o: ../src/main.c \
+  /home/atcsol/workspace/rtems_sctre/task_management_rtems/include/rtems_config.h \
+  /home/atcsol/workspace/rtems_sctre/task_management_rtems/include/consume_ticks.h
+
+/home/atcsol/workspace/rtems_sctre/task_management_rtems/include/rtems_config.h:
+
+/home/atcsol/workspace/rtems_sctre/task_management_rtems/include/consume_ticks.h:
